2be3879a96dcf5002e5c9e74626d723962c2e8e5
remymandit <remy.mandit@free.fr>
2014-07-30 16:41:48 +0200

    add configuration dependency injection userBundle

---
+++ src/DeclareNounou/UserBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace DeclareNounou\UserBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder() {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('declare_nounou_user');
+        
+        // Here you should define the parameters that are allowed to
+        // configure your bundle. See the documentation linked above for
+        // more information on that topic.
+        $rootNode->children()
+                ->end();
+        
+        return $treeBuilder;
+    }
+}
+
